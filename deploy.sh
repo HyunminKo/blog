1ac9004f1df8db7bc4020ae4ddaea7781f921af3
HyunminKo <hyunmin.ko4578@gmail.com>
2021-06-13 01:32:07 +0900

    rebuilding site Sun Jun 13 01:32:07 KST 2021

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+hugo -t docport
+
+cd public
+
+git add .
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+    then msg="$1"
+fi
+git commit -m "$msg"
+git push origin master
+
+cd ..
+
+git add .
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+    then msg="$1"
+fi
+git commit -m "$msg"
+git push origin master
